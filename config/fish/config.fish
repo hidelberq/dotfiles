3185e61e3ee8684effe7ec9b0d7ea31721bd87b3
hidelberq <hide.seaweed@gmail.com>
2019-01-17 01:25:56 +0900

    Add fish and git

---
+++ config/fish/config.fish
@@ -0,0 +1,3 @@
+bind \cr peco_select_history # Bind for peco select history to Ctrl+R
+bind \c] peco_change_directory # Bind for prco change directory to Ctrl+]
+
--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 dir_path=`pwd`
 
-ln -sf $dir_path/.vimrc ~/.vimrc
-ln -sf $dir_path/.tmux.conf ~/.tmux.conf
+ln -sf $dir_path/vimrc ~/.vimrc
+ln -sf $dir_path/tmux.conf ~/.tmux.conf
+ln -sf $dir_path/config/fish ~/.config/fish
+ln -sf $dir_path/config/git ~/.config/git
 
