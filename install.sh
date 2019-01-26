e2a0739dd708f702e408cfc711097e912e29919d
hidelberq <hide.seaweed@gmail.com>
2019-01-26 18:02:33 +0900

    Add ignore rule

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ ln -sfvn $dir_path/todo.cfg ~/.todo.cfg
 
 ln -sfvn $dir_path/config/fish ~/.config/fish
 ln -sfvn $dir_path/config/git ~/.config/git
+ln -sfvn $dir_path/config/karabiner ~/.config/karabiner
+
